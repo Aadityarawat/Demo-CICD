ffd5e091c8f6144827455a163645f0e4a2a3b5e8
Aditya Rawat <aditya.r@kitlabs.tech>
2024-09-24 12:21:47 +0530

    second commit

--- app/src/main/java/com/example/democicd/MainActivity.kt
+++ app/src/main/java/com/example/democicd/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.democicd
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("Hello world","")
 
     }
 }
