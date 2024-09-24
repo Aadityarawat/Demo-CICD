9c088475166d13ad063a370e5e28571f4e485f51
Aditya Rawat <aditya.r@kitlabs.tech>
2024-09-24 12:30:15 +0530

    third commit

--- app/src/main/java/com/example/democicd/MainActivity.kt
+++ app/src/main/java/com/example/democicd/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("Hello world","")
+        Log.d("Hello world","hello")
 
     }
 }
